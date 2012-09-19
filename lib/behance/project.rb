ad43dae2aa5ee62d25f19acf96e6d96774552118
Amed Rodriguez <amed@tractical.com>
2012-09-19 13:30:32 -0500

    improved methods documentation

--- lib/behance/project.rb
+++ lib/behance/project.rb
@@ -4,23 +4,29 @@ module Behance
     module Project
       # Public: Search for projects.
       # 
-      # q       - Free text query string.
-      # sort    - The order the results are returned in. 
-      #           Possible values: featured_date (default), appreciations, views, comments, 
-      #           published_date.
-      # time    - Limits the search by time. 
-      #           Possible values: all (default), today, week, month.
-      # field   - Limits the search by creative field. 
-      #           Accepts a URL-encoded field name from the list of defined creative fields.
-      # country - Limits the search by a 2-letter FIPS country code.
-      # state   - Limits the search by state or province name.
-      # city    - Limits the search by city name.
-      # page    - The page number of the results, always starting with 1.
-      # tags    - Limits the search by tags. 
-      #           Accepts one tag name or a pipe-separated list of tag names.
+      # options - The Hash of options that the API would expect:
+      #           :q       - Free text query string.
+      #           :sort    - The order the results are returned in. 
+      #                      Possible values: featured_date (default), apprecia-
+      #                      tions, views, comments, 
+      #                      published_date.
+      #           :time    - Limits the search by time. 
+      #                      Possible values: all (default), today, week, month.
+      #           :field   - Limits the search by creative field. 
+      #                      Accepts a URL-encoded field name from the list of 
+      #                      defined creative fields.
+      #           :country - Limits the search by a 2-letter FIPS country code.
+      #           :state   - Limits the search by state or province name.
+      #           :city    - Limits the search by city name.
+      #           :page    - The page number of the results, always starting 
+      #                      with 1.
+      #           :tags    - Limits the search by tags. 
+      #                      Accepts one tag name or a pipe-separated list of 
+      #                      tag names.
       #
       # Examples
       # 
+      #   @client.projects
       #   @client.projects(q: "Freelance", state: "CA", field: "Branding")
       #
       # Returns an array of projects in JSON format.
@@ -44,15 +50,18 @@ module Behance
       # Public: Get the comments for a project
       # 
       # project_id - The ID (Integer) of the project. 
-      # page       - The page number of the results, always starting with 1.
+      # options    - The Hash of options that the API would expect:
+      #              :page - The page number of the results, always 
+      #              starting with 1.
       # 
       # Examples
       # 
+      #   @client.project_comments(1)
       #   @client.project_comments(1, page: 1)
       # 
       # Returns an array of project comments in JSON format.
-      def project_comments(project_id)
-        request("project_comments/#{project_id}")["comments"] 
+      def project_comments(project_id, options={})
+        request("project_comments/#{project_id}", options)["comments"] 
       end
     end
   end
--- lib/behance/user.rb
+++ lib/behance/user.rb
@@ -4,22 +4,28 @@ module Behance
     module User
       # Public: Search for users.
       #
-      # q       - Free text query string.
-      # field   - Limits the search by creative field. 
-      #           Accepts a URL-encoded field name from the list of defined creative fields.
-      # country - Limits the search by a 2-letter FIPS country code.
-      # state   - Limits the search by state or province name.
-      # city    - Limits the search by city name.
-      # page    - The page number of the results, always starting with 1.
-      # sort    - The order the results are returned in. 
-      #           Possible values: featured_date (default), appreciations, views, comments, 
-      #           published_date, followed.
-      # tags    - Limits the search by tags. 
-      #           Accepts one tag name or a pipe-separated list of tag names.
+      # options - The Hash of options that the API would expect:
+      #           :q       - Free text query string.
+      #           :field   - Limits the search by creative field. 
+      #                      Accepts a URL-encoded field name from the list of 
+      #                      defined creative fields.
+      #           :country - Limits the search by a 2-letter FIPS country code.
+      #           :state   - Limits the search by state or province name.
+      #           :city    - Limits the search by city name.
+      #           :page    - The page number of the results, always starting 
+      #                      with 1.
+      #           :sort    - The order the results are returned in. 
+      #                      Possible values: featured_date (default), apprecia-
+      #                      tions, views, comments, 
+      #                      published_date, followed.
+      #           :tags    - Limits the search by tags. 
+      #                      Accepts one tag name or a pipe-separated list of 
+      #                      tag names.
       #
       # Examples
       #
-      #   @client.users(q: "Juan", state: "California", :tags "freelance,web")
+      #   @client.users
+      #   @client.users(q: "Juan", state: "California", :tags "freelance")
       #
       # Returns an array of users in JSON format.
       def users(options={})
@@ -30,6 +36,11 @@ module Behance
       #
       # user - can be an ID (Integer) or username (String).
       #
+      # Examples
+      #
+      #   @client.user(1)
+      #   @client.user("foo")
+      #
       # Returns a single user object.
       def user(user)
         request("user/#{user}")["user"]
@@ -37,13 +48,21 @@ module Behance
 
       # Public: Get the projects published by an user. 
       #
-      # user - Can be an ID (Integer) or username (String)
-      # sort - The order the results are returned in. 
-      #        Possible values: featured_date (default), appreciations, views, 
-      #        comments, published_date.
-      # time - Limits the search by time. 
-      #        Possible values: all (default), today, week, month.
-      # page - The page number of the results, always starting with 1.
+      # user    - Can be an ID (Integer) or username (String)
+      # options - The Hash of options that the API would expect:
+      #           :sort - The order the results are returned in. 
+      #                   Possible values: featured_date (default), apprecia-
+      #                   tions, views, comments, published_date.
+      #           :time - Limits the search by time. 
+      #                   Possible values: all (default), today, week, month.
+      #           :page - The page number of the results, always starting 
+      #                   with 1.
+      #
+      # Examples
+      #
+      #   @client.user_projects(1)
+      #   @client.user_projects("foo")
+      #   @client.user_projects("foo", page: 2, sort: "views")
       #
       # Returns an array of projects published an user in JSON format.
       def user_projects(user, options={})
@@ -52,13 +71,21 @@ module Behance
 
       # Public: Get the works-in-progress published by a user.
       #
-      # user - can be an ID (Integer) or username (String).
-      # sort - The order the results are returned in. 
-      #        Possible values: featured_date (default), appreciations, views, 
-      #        comments, published_date.
-      # time - Limits the search by time. Possible values: all (default), 
-      #        today, week, month.
-      # page - The page number of the results, always starting with 1.
+      # user    - can be an ID (Integer) or username (String).
+      # options - The Hash of options that the API would expect:
+      #           :sort - The order the results are returned in.
+      #                   Possible values: featured_date (default), apprecia-
+      #                   tions, views, comments, published_date.
+      #           :time - Limits the search by time. Possible values: all 
+      #                   (default), today, week, month.
+      #           :page - The page number of the results, always starting 
+      #                   with 1.
+      #
+      # Examples
+      #
+      #   @client.user_wips(1)
+      #   @client.user_wips(1, page: 2)
+      #   @client.user_wips("foo", sort: "comments", page: 3)
       #
       # Returns an array of work-in-progress of an user in JSON format.
       def user_wips(user, options={})
@@ -68,6 +95,11 @@ module Behance
       # Public: Get a list of user's recently appreciated projects.
       #
       # user - can be an ID (Integer) or username (String).
+      #
+      # Examples
+      #
+      #   @client.user_appreciations(1)
+      #   @client.user_appreciations("foo")
       #
       # Returns an array of user's recently appreciated projects in JSON
       # format.
